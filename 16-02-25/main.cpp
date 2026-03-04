f533aa2d3d1830b8a3a08c661f6e76d3c36a797f
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-03-04 23:53:12 +0300

    skeleton and constructor and destructor

--- 16-02-25/main.cpp
+++ 16-02-25/main.cpp
@@ -8,6 +8,28 @@ struct BiList
   BiList<T> *prev;
 };
 
+template <class T>
+class DoublyLinkedList
+{
+private:
+  BiList<T> *fake;
+  size_t sz;
+
+public:
+  DoublyLinkedList()
+  {
+    fake = new BiList<T>();
+    fake->next = fake;
+    fake->prev = fake;
+    sz = 0;
+  }
+
+  ~DoublyLinkedList()
+  {
+    delete fake;
+  }
+};
+
 int main()
 {
 }
